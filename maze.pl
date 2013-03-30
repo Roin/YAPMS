518d9a7e9a508560d3064b0c0e9a2a41bea692b1
Florian Kanngießer <florian.kanngiesser@yahoo.de>
2013-03-30 16:22:06 +0100

    Did some work

--- maze.pl
+++ maze.pl
@@ -3,13 +3,16 @@ finish(u).
 route(a,g).
 route(g,l).
 route(l,s).
-route(s,a).
-route(l,u).
+route(s,u).
 travel(A,A).
 travel(A,C) :- route(A,B),travel(B,C).
-solve :- start(A),finish(B), travel(A,B).
+%solve :- start(A),finish(B), travel(A,B).
 travellog(A,A,[]).
 travellog(A,C,[A-B|Steps]) :-
 route(A,B), travellog(B,C,Steps).
 solve(L) :- start(A), finish(B),
 travellog(A,B,L).
+travelsafe(A,A,_).
+travelsafe(A,C,Closed) :-
+route(A,B), \+ member(B,Closed),
+travelsafe(B,C,[B|Closed]).
