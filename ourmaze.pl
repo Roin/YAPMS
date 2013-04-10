55b88fed3384d0123dd151bacb80e52798ee26d5
Watnuss <watnuss@gmx.de>
2013-04-10 15:43:16 +0200

    move east functionality

--- ourmaze.pl
+++ ourmaze.pl
@@ -3,45 +3,93 @@
 %findpath(MAZELIST, START, WALKABLE, X).
 
 maze(MAZELIST, START, ZIEL, X) :-
-    move(MAZELIST, START, ZIEL, X).
+    check(MAZELIST, START, ZIEL, VISITED, X).
 
-move(MAZELIST, (CurRow, CurColumn), (ZIEL1, ZIEL2), X) :-
-    CurRow =:= ZIEL1,
-    CurColumn =:= ZIEL2,
-    getValue(MAZELIST, (CurRow, CurColumn) , VALUE),
+% TODO maybe add X to VISITED
+check(MAZELIST, (CURROW, CURCOLUMN), (ZIEL1, ZIEL2), VISITED, X) :-
+    CURROW =:= ZIEL1,
+    CURCOLUMN =:= ZIEL2,
+    getValue(MAZELIST, (CURROW, CURCOLUMN) , VALUE),
     VALUE =:= 0,
-    X = (CurRow, CurColumn).
-
-moveeast(X) :-true.
-
-movesouth(X) :-true.
-
-movewest(X) :-true.
-
-movenorth(X) :-true.
+    X = (CURROW, CURCOLUMN).
 
 
-findpath(MAZELIST, START, WALKABLE, X) :-
-    %    StartRow = START1,
-    %    StartColumn = START2,
-    getValue(MAZELIST, START, X).
+% check east unvisited
+check(MAZELIST, (CURROW, CURCOLUMN), ZIEL, VISITED, X) :-
+    CURCOLUMNI is CURCOLUMN+1,
+    getValue(MAZELIST, (CURROW, CURCOLUMNI) , VALUE),
+    VALUE =:= 0,
+    isNotVisited(CURROW, CURCOLUMNI, VISITED),
+    move(MAZELIST, (CURROW, CURCOLUMNI), ZIEL, VISITED, X).
+
+
+% check south unvisited
+
+
+
+% check west unvisited
+
+
+% check north unvisited
+
+
+% check west visited
+
+
+% check south visited
+
+
+% check east visited
+
+
+
+% check north visited
+
+
+move(MAZELIST, (CURROW, CURCOLUMN), ZIEL, VISITED, X) :-
+    maze(MAZELIST, (CURROW, CURCOLUMN), ZIEL, [(CURROW, CURCOLUMN)|VISITED], X).
+    
+
+
+
+
+
+
+%findpath(MAZELIST, START, WALKABLE, X) :-
+%    STARTROW = START1,
+%    STARTCOLUMN = START2,
+%    getValue(MAZELIST, START, X).
+
+isNotVisited(ROW, COLUMN, VISITED) :-
+    \+isVisited(ROW, COLUMN, VISITED).
+
+isVisited(ROW, COLUMN, []) :-
+    false.
+
+isVisited(ROW, COLUMN, [(X,Y)|VISITED]) :-
+    ROW =:= X,
+    COLUMN =:= Y.
+
+isVisited(ROW, COLUMN, [_|VISITED]) :-
+    isVisited(ROW, COLUMN, VISITED).
+
 
 getValue(MAZELIST, START, X) :-
     inRow(MAZELIST, 1, 1, START, X).
 
-inRow([Row|_], CurRow, CurColumn, (StartRow, StartColumn), X) :-
-    CurRow =:= StartRow,
-    inColumn(Row, CurRow, CurColumn, (StartRow, StartColumn), X).
+inRow([Row|_], CURROW, CURCOLUMN, (STARTROW, STARTCOLUMN), X) :-
+    CURROW =:= STARTROW,
+    inColumn(Row, CURROW, CURCOLUMN, (STARTROW, STARTCOLUMN), X).
 
-inRow([_|RestMaze], CurRow, CurColumn, (StartRow, StartColumn), X) :-
-    CurRow < StartRow,
-    CurRowI is CurRow+1,
-    inRow(RestMaze, CurRowI, CurColumn, (StartRow, StartColumn), X).
+inRow([_|RestMaze], CURROW, CURCOLUMN, (STARTROW, STARTCOLUMN), X) :-
+    CURROW < STARTROW,
+    CURROWI is CURROW+1,
+    inRow(RestMaze, CURROWI, CURCOLUMN, (STARTROW, STARTCOLUMN), X).
 
-inColumn([Element|_], CurRow, CurColumn, (StartRow, StartColumn), Element) :- 
-    CurColumn =:= StartColumn.
+inColumn([Element|_], CURROW, CURCOLUMN, (STARTROW, STARTCOLUMN), Element) :- 
+    CURCOLUMN =:= STARTCOLUMN.
 
-inColumn([_|RestList], CurRow, CurColumn, (StartRow, StartColumn), X) :-
-    CurColumn < StartColumn,
-    CurColumnI is CurColumn+1,
-    inColumn(RestList, CurRow, CurColumnI, (StartRow, StartColumn), X).
+inColumn([_|RestList], CURROW, CURCOLUMN, (STARTROW, STARTCOLUMN), X) :-
+    CURCOLUMN < STARTCOLUMN,
+    CURCOLUMNI is CURCOLUMN+1,
+    inColumn(RestList, CURROW, CURCOLUMNI, (STARTROW, STARTCOLUMN), X).
